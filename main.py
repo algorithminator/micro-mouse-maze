e5d1f416e7eb97d4177d37ea98af7e9347bc7b28
Frederic Dorn <83763956+algorithminator@users.noreply.github.com>
2026-03-23 16:13:09 +0100

    show coordinates

--- main.py
+++ main.py
@@ -1,59 +1,102 @@
+def get_coordinates():
+    global degrees, counterY, counterX
+    
+    degrees = input.compass_heading()
+    if degrees < 45:
+        counterY = counterY + 1
+        basic.show_number(counterX)
+        basic.show_number(counterY)
+    elif degrees < 135:
+        counterX = counterX + 1
+        basic.show_number(counterX)
+        basic.show_number(counterY)
+    elif degrees < 225:
+        counterY = counterY - 1
+        basic.show_number(counterX)
+        basic.show_number(counterY)
+    elif degrees < 315:
+        counterX = counterX - 1
+        basic.show_number(counterX)
+        basic.show_number(counterY)
+    else:
+        counterY = counterY + 1
+        basic.show_number(counterX)
+        basic.show_number(counterY)
+    #return [counterX, counterY]
+# 0 - forward , 1- right , 2 - left, 4 -backward
+def show_direction():
+    global degrees, counterY, counterX
+    degrees = input.compass_heading()
+    if degrees < 45:
+        basic.show_arrow(ArrowNames.NORTH)
+        counterY = counterY + 1
+    elif degrees < 135:
+        basic.show_arrow(ArrowNames.EAST)
+        counterX = counterX + 1
+    elif degrees < 225:
+        basic.show_arrow(ArrowNames.SOUTH)
+        counterY = counterY - 1
+    elif degrees < 315:
+        basic.show_arrow(ArrowNames.WEST)
+        counterX = counterX - 1
+    else:
+        basic.show_arrow(ArrowNames.NORTH)
+        counterY = counterY + 1
+whichWay = 0
+counterX = 0
+counterY = 0
+degrees = 0
+# signX = 1
+# signY = 1
+bitbot.enable_pid(False)
 leftSensor = 1023
 rightSensor = 1023
 centerSensor = 1023
-alongWall = Math.random_boolean()
-spinDir = BBRobotDirection.LEFT
-bitbot.enable_pid(False)
 
 def on_forever():
-    global rightSensor, leftSensor, centerSensor, spinDir, alongWall
-    centerSensor = 1023
+    global rightSensor, leftSensor, centerSensor, whichWay, counterX, counterY
     rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
-    # - 40 / 10
     leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
-    # - 40 / 10
     centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
-    # - 40 / 10 input.running_time_micros()
-
-    if alongWall == True:
-        spinDir = BBRobotDirection.LEFT
-    else:
-        spinDir = BBRobotDirection.RIGHT
-    
-    while centerSensor <= 50:
-        bitbot.spin_deg(spinDir, 60, 90)
-        basic.pause(50)
+    basic.pause(100)
+    if leftSensor <= 100 and rightSensor <= 100:
+        # "forward"
+        if whichWay == 0:
+            bitbot.gocm(BBDirection.FORWARD, 30, 5)
+            basic.pause(100)
+            #counter = counter + 1
+            # led.plot(counterX, counterY)
+            # show_direction();
+            get_coordinates()
+            #basic.pause(100)
+        # "right"
+        if whichWay == 1:
+            bitbot.spin_deg(BBRobotDirection.RIGHT, 30, 30)
+            whichWay = 0
+            basic.pause(100)
+            rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
+            centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
+            if rightSensor > 100 and centerSensor > 100:
+                bitbot.spin_deg(BBRobotDirection.LEFT, 30, 60)
+                basic.pause(100)
+                leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
+                centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
+                if leftSensor > 100 and centerSensor > 100:
+                    bitbot.spin_deg(BBRobotDirection.LEFT, 30, 150)
+                    basic.pause(100)
+        rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
+        leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
         centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
- 
-    
-    rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
-    if rightSensor < 50:
-        while rightSensor < 50:
-           alongWall = Math.random_boolean()
-           bitbot.spin_deg(BBRobotDirection.LEFT, 60, 30)
-           basic.pause(50)
-           rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
-        bitbot.move(BBMotor.LEFT, BBDirection.FORWARD, 80)
-        bitbot.move(BBMotor.RIGHT, BBDirection.FORWARD, 70)
-        basic.pause(50)
-        # # langs veggen
-        #bitbot.spin_deg(BBRobotDirection.RIGHT, 60, 15)
-    
-    leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
-    if leftSensor < 50:
-        while leftSensor < 50:
-            alongWall = Math.random_boolean()
-            bitbot.spin_deg(BBRobotDirection.RIGHT, 60, 30)
-            basic.pause(50)
-            leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
-        bitbot.move(BBMotor.LEFT, BBDirection.FORWARD, 70)
-        bitbot.move(BBMotor.RIGHT, BBDirection.FORWARD, 80)
-        basic.pause(50)
-        # # langs veggen
-        #bitbot.spin_deg(BBRobotDirection.LEFT, 60, 15)
-
-    bitbot.move(BBMotor.LEFT, BBDirection.FORWARD, 80)
-    bitbot.move(BBMotor.RIGHT, BBDirection.FORWARD, 75)
-    basic.pause(50)
-               
+        basic.pause(100)
+    if leftSensor > 100 and rightSensor > 100 and centerSensor > 100:
+        bitbot.gocm(BBDirection.REVERSE, 30, 12)
+        whichWay = 1
+        basic.pause(100)
+        # bitbot.spin_deg(BBRobotDirection.RIGHT, 25, 90)
+        rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
+        leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
+        centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
+        basic.pause(100)
+    bitbot.steer(bitbot.merge_line_position(), 30)
+    basic.pause(100)
 basic.forever(on_forever)
