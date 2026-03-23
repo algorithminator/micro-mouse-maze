d04d0d5e742a82b78d0778ec4c442c83c96da306
Frederic Dorn <83763956+algorithminator@users.noreply.github.com>
2026-03-23 16:18:14 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,54 +1,59 @@
-whichWay = 0
-counter = 0
-bitbot.enable_pid(False)
 leftSensor = 1023
 rightSensor = 1023
 centerSensor = 1023
-# 0 - forward , 1- right , 2 - left, 4 -backward
+alongWall = Math.random_boolean()
+spinDir = BBRobotDirection.LEFT
+bitbot.enable_pid(False)
 
 def on_forever():
-    global rightSensor, leftSensor, centerSensor, whichWay, counter
+    global rightSensor, leftSensor, centerSensor, spinDir, alongWall
+    centerSensor = 1023
     rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
+    # - 40 / 10
     leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
+    # - 40 / 10
     centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
-    basic.pause(100)
-    if leftSensor <= 50 and rightSensor <= 50:
-        
-        # "forward"
-        if whichWay == 0:
-           bitbot.gocm(BBDirection.FORWARD, 30, 5)
-           counter = counter + 1
-           #basic.show_number(counter)
-           basic.pause(100)
-        #"right"
-        if whichWay == 1:
-            bitbot.spin_deg(BBRobotDirection.RIGHT, 30, 30)
-            whichWay = 0
-            basic.pause(100)
-            rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
-            centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
-            if  rightSensor > 100 and centerSensor > 100:
-                bitbot.spin_deg(BBRobotDirection.LEFT, 30, 60)
-                basic.pause(100)
-                leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
-                centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
-                if  leftSensor > 100 and centerSensor > 100:
-                        bitbot.spin_deg(BBRobotDirection.LEFT, 30, 150)
-                        basic.pause(100)
+    # - 40 / 10 input.running_time_micros()
 
-        rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
-        leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
+    if alongWall == True:
+        spinDir = BBRobotDirection.LEFT
+    else:
+        spinDir = BBRobotDirection.RIGHT
+    
+    while centerSensor <= 50:
+        bitbot.spin_deg(spinDir, 60, 90)
+        basic.pause(50)
         centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
-        basic.pause(100)
-    if leftSensor > 100 and rightSensor > 100 and centerSensor > 100:
-        bitbot.gocm(BBDirection.REVERSE, 30, 12)
-        whichWay = 1
-        basic.pause(100)
-        # bitbot.spin_deg(BBRobotDirection.RIGHT, 25, 90)
-        rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
-        leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
-        centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
-        basic.pause(100)
-    bitbot.steer(bitbot.merge_line_position(), 30)
-    basic.pause(100)
+ 
+    
+    rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
+    if rightSensor < 50:
+        while rightSensor < 50:
+           alongWall = Math.random_boolean()
+           bitbot.spin_deg(BBRobotDirection.LEFT, 60, 30)
+           basic.pause(50)
+           rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
+        bitbot.move(BBMotor.LEFT, BBDirection.FORWARD, 80)
+        bitbot.move(BBMotor.RIGHT, BBDirection.FORWARD, 70)
+        basic.pause(50)
+        # # langs veggen
+        #bitbot.spin_deg(BBRobotDirection.RIGHT, 60, 15)
+    
+    leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
+    if leftSensor < 50:
+        while leftSensor < 50:
+            alongWall = Math.random_boolean()
+            bitbot.spin_deg(BBRobotDirection.RIGHT, 60, 30)
+            basic.pause(50)
+            leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
+        bitbot.move(BBMotor.LEFT, BBDirection.FORWARD, 70)
+        bitbot.move(BBMotor.RIGHT, BBDirection.FORWARD, 80)
+        basic.pause(50)
+        # # langs veggen
+        #bitbot.spin_deg(BBRobotDirection.LEFT, 60, 15)
+
+    bitbot.move(BBMotor.LEFT, BBDirection.FORWARD, 80)
+    bitbot.move(BBMotor.RIGHT, BBDirection.FORWARD, 75)
+    basic.pause(50)
+               
 basic.forever(on_forever)
