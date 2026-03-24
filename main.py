44cb97abb8719bd31c737cc5f6156a0fdfa8631c
Frederic Dorn <83763956+algorithminator@users.noreply.github.com>
2026-03-24 10:22:34 +0100

    Restore '23.3.2026, 16:13:09 show coordinates'

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 def get_coordinates():
-    global degrees, counterY, counterX, listVisitedDir
+    global degrees, counterY, counterX
+    
     degrees = input.compass_heading()
     if degrees < 45:
         counterY = counterY + 1
-        listVisitedDir.push("N")
+        basic.show_number(counterX)
+        basic.show_number(counterY)
     elif degrees < 135:
         counterX = counterX + 1
-        listVisitedDir.push("E")
+        basic.show_number(counterX)
+        basic.show_number(counterY)
     elif degrees < 225:
         counterY = counterY - 1
-        listVisitedDir.push("S")
+        basic.show_number(counterX)
+        basic.show_number(counterY)
     elif degrees < 315:
         counterX = counterX - 1
-        listVisitedDir.push("W")
+        basic.show_number(counterX)
+        basic.show_number(counterY)
     else:
         counterY = counterY + 1
-        listVisitedDir.push("N")
-
+        basic.show_number(counterX)
+        basic.show_number(counterY)
+    #return [counterX, counterY]
+# 0 - forward , 1- right , 2 - left, 4 -backward
 def show_direction():
     global degrees, counterY, counterX
     degrees = input.compass_heading()
@@ -35,44 +42,10 @@ def show_direction():
     else:
         basic.show_arrow(ArrowNames.NORTH)
         counterY = counterY + 1
-
-def checkIfVisited():
-    for i in range(listVisitedX.length):
-        if (counterX == listVisitedX.get(i)) and (counterY == listVisitedY.get(i)):
-            return True
-            #basic.show_string("Visited")
-    return False
-
-def getPreviousVisited():
-    for i in range(listVisitedX.length):
-        if i > 0:
-           if (counterX == listVisitedX.get(i)) and (counterY == listVisitedY.get(i)):
-              return listVisitedX.get(i-1), listVisitedY.get(i-1)
-              #basic.show_string("Visited")
-        else:
-            basic.show_string("Start")
-            return 0,0
-    return 0,0
-
-def go_back():
-    bitbot.gocm(BBDirection.REVERSE, 30, 12)
-    whichWay = 1
-    basic.pause(100)
-    # bitbot.spin_deg(BBRobotDirection.RIGHT, 25, 90)
-    rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
-    leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
-    centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
-    basic.pause(100)
-
-
 whichWay = 0
 counterX = 0
 counterY = 0
 degrees = 0
-listVisitedX: List[number] = []
-listVisitedY: List[number] = []
-listVisitedDir: List[string] = []
-backtracking = False
 # signX = 1
 # signY = 1
 bitbot.enable_pid(False)
@@ -81,33 +54,21 @@ rightSensor = 1023
 centerSensor = 1023
 
 def on_forever():
-    global rightSensor, centerSensor, leftSensor, whichWay
+    global rightSensor, leftSensor, centerSensor, whichWay, counterX, counterY
     rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
-    centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
     leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
-    # basic.show_number(rightSensor)
-    # basic.show_number(centerSensor)
-    # basic.show_number(leftSensor)
+    centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
     basic.pause(100)
-    if leftSensor <= 50 and rightSensor <= 50:
+    if leftSensor <= 100 and rightSensor <= 100:
         # "forward"
         if whichWay == 0:
             bitbot.gocm(BBDirection.FORWARD, 30, 5)
-
             basic.pause(100)
+            #counter = counter + 1
+            # led.plot(counterX, counterY)
+            # show_direction();
             get_coordinates()
-            basic.pause(100)
-            if (checkIfVisited()):
-                go_back()
-                backtracking = True
-                prevX,prevY = getPreviousVisited()
-                
-                  
-
-            basic.pause(100)
-            listVisitedX.push(counterX)
-            listVisitedY.push(counterY)
-        # basic.pause(100)
+            #basic.pause(100)
         # "right"
         if whichWay == 1:
             bitbot.spin_deg(BBRobotDirection.RIGHT, 30, 30)
@@ -122,15 +83,20 @@ def on_forever():
                 centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
                 if leftSensor > 100 and centerSensor > 100:
                     bitbot.spin_deg(BBRobotDirection.LEFT, 30, 150)
-                    #backtracking = True
-                    listVisitedDir.pop()
                     basic.pause(100)
         rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
         leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
         centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
         basic.pause(100)
     if leftSensor > 100 and rightSensor > 100 and centerSensor > 100:
-        go_back()
+        bitbot.gocm(BBDirection.REVERSE, 30, 12)
+        whichWay = 1
+        basic.pause(100)
+        # bitbot.spin_deg(BBRobotDirection.RIGHT, 25, 90)
+        rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
+        leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
+        centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
+        basic.pause(100)
     bitbot.steer(bitbot.merge_line_position(), 30)
     basic.pause(100)
 basic.forever(on_forever)
