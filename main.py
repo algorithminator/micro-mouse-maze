a2d691bd6566f54b071fc287835d2d8c8733fa1e
Frederic Dorn <83763956+algorithminator@users.noreply.github.com>
2026-03-26 21:55:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,40 @@
-whichWay = 0
-counter = 0
-bitbot.enable_pid(False)
-leftSensor = 1023
-rightSensor = 1023
-centerSensor = 1023
-# 0 - forward , 1- right , 2 - left, 4 -backward
+DISTANCE_TO_WALL = 50
+
+
+def adjustDirToCompass(degrees2):
+    
+    degrees1 = input.compass_heading()  
+    diffDeg = (degrees1 -degrees2)%360
+    #less to the left than..
+    if diffDeg >=  270 :
+            adjustDir = -3 
+    #..to the right      
+    elif diffDeg <= 90:
+            adjustDir = +5 
+            
+    else:
+            
+            adjustDir = 0
+            
+    
+    return adjustDir
 
 def on_forever():
-    global rightSensor, leftSensor, centerSensor, whichWay, counter
-    rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
-    leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
-    centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
-    basic.pause(100)
-    if leftSensor <= 50 and rightSensor <= 50:
-        
-        # "forward"
-        if whichWay == 0:
-           bitbot.gocm(BBDirection.FORWARD, 30, 5)
-           counter = counter + 1
-           #basic.show_number(counter)
-           basic.pause(100)
-        #"right"
-        if whichWay == 1:
-            bitbot.spin_deg(BBRobotDirection.RIGHT, 30, 30)
-            whichWay = 0
-            basic.pause(100)
-            rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
-            centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
-            if  rightSensor > 100 and centerSensor > 100:
-                bitbot.spin_deg(BBRobotDirection.LEFT, 30, 60)
-                basic.pause(100)
-                leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
-                centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
-                if  leftSensor > 100 and centerSensor > 100:
-                        bitbot.spin_deg(BBRobotDirection.LEFT, 30, 150)
-                        basic.pause(100)
+    global DISTANCE_TO_WALL 
+    degrees2 = input.compass_heading()
 
-        rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
-        leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
-        centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
+    distWall = bitbot.sonar(BBPingUnit.CENTIMETERS)
+    basic.pause(100)
+    
+    # --WALL UP FRONT?--#
+    while distWall <= DISTANCE_TO_WALL :
+        bitbot.stop(BBStopMode.BRAKE)
         basic.pause(100)
-    if leftSensor > 100 and rightSensor > 100 and centerSensor > 100:
-        bitbot.gocm(BBDirection.REVERSE, 30, 12)
-        whichWay = 1
-        basic.pause(100)
-        # bitbot.spin_deg(BBRobotDirection.RIGHT, 25, 90)
-        rightSensor = bitbot.read_line_analog(BBPLineSensor.RIGHT)
-        leftSensor = bitbot.read_line_analog(BBPLineSensor.LEFT)
-        centerSensor = bitbot.read_line_analog(BBPLineSensor.CENTRE)
-        basic.pause(100)
-    bitbot.steer(bitbot.merge_line_position(), 30)
+        bitbot.spin_deg(BBRobotDirection.LEFT, 60, 90)
+        basic.pause(200)
+        distWall = bitbot.sonar(BBPingUnit.CENTIMETERS)
+    
+    # -- KEEP Direction checking compass-- #
+    bitbot.steer(adjustDirToCompass(degrees2), 80)
     basic.pause(100)
 basic.forever(on_forever)
